5e8f02aed84d91599aaf5eaac9d3532f8f92fd48
sn0re <david@code0.xyz>
2017-11-07 22:29:49 +0100

    asd

---
+++ GameOver.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Button_mouse_entered():
+	get_node("ButRestart/ImgRestart").modulate = "d84848"
+	print("entered")
+	
+
+func _on_Button_pressed():
+	print("pressed")
+	emit_signal("restartGame")
+
+
+func _on_Button_mouse_exited():
+	get_node("ButRestart/ImgRestart").modulate = "ffffff"
+	print("exited")
