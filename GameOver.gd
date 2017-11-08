2d89b62ae06517dd7849b664109dfa52264b71af
sn0re <david@code0.xyz>
2017-11-08 06:04:12 +0100

    asd7

--- GameOver.gd
+++ GameOver.gd
@@ -7,13 +7,18 @@ signal restartGame
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	pass
+#	pass
+	set_process_input(true)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 
+
+func _input(event):
+	if event.is_action_pressed("ui_enter") and visible:
+		emit_signal("restartGame")
 
 func _on_Button_mouse_entered():
 	get_node("ButRestart/ImgRestart").modulate = "d84848"
