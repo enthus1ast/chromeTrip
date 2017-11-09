fe92eb84ff8f0756e4d4f3de2a79aeae18e769dd
sn0re <david@code0.xyz>
2017-11-09 04:49:38 +0100

    fix color bug

--- MainMenu.gd
+++ MainMenu.gd
@@ -35,3 +35,10 @@ func _ready():
 
 func _on_ButtonQuit_pressed():
 	get_tree().quit()
+
+
+func _on_ButtonNewGame_pressed():
+	pass # replace with function body
+	self.hide()
+	get_tree().get_root().get_node("Control/menu/networkPanel").show()
+	
