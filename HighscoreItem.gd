994b867b335907cd6cb0464ee280d87200e689ac
sn0re <david@code0.xyz>
2017-11-10 03:11:20 +0100

    :=)

--- HighscoreItem.gd
+++ HighscoreItem.gd
@@ -3,12 +3,10 @@ extends HBoxContainer
 func fill(rank, score, team):
 	get_node("Rank").text = str(rank)
 	get_node("Score").text = str(score)
-	
 	var teamline = ""
 	for player in team:
 		var one = utils.computeColorBB( player, player ) + " "
 		teamline += one 
-		
 	print(str(teamline))
 	get_node("Team").set_bbcode(str(teamline))
 
