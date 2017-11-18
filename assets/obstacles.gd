ca7ec97fa1775ff2160ee0f278d105983ec24964
Th0r <Th0r@code0.xyz>
2017-11-18 18:09:24 +0100

    Signed-off-by: Th0r <Th0r@code0.xyz>

--- assets/obstacles.gd
+++ assets/obstacles.gd
@@ -4,7 +4,6 @@ func choice(_class,_id):
 	get_node(_class+str(_id)).pause_mode=0
 	get_node(_class+str(_id)).set_visible(true)
 	get_node(_class+str(_id)).get_node("obstacleShape").disabled=false
-	
 func _ready():
 	for child in get_children():
 		if typeof(child)==typeof(KinematicBody2D):
